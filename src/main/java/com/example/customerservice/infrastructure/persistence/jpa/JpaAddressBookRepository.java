817be06e64acb84779ccf2f23cd49801e13459d2
Stanislav Dobrovolschi <st.dobrovolschi@gmail.com>
2017-11-18 16:37:25 +0200

    Implemented add AddressBook repository's method.

--- src/main/java/com/example/customerservice/infrastructure/persistence/jpa/JpaAddressBookRepository.java
+++ src/main/java/com/example/customerservice/infrastructure/persistence/jpa/JpaAddressBookRepository.java
@@ -3,7 +3,10 @@ package com.example.customerservice.infrastructure.persistence.jpa;
 import com.example.customerservice.domain.model.AddressBook;
 import com.example.customerservice.domain.model.AddressBookRepository;
 import com.example.customerservice.domain.model.CustomerId;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
 
 /**
  * @author Stanislav Dobrovolschi
@@ -11,9 +14,15 @@ import org.springframework.stereotype.Repository;
 @Repository("addressBookRepository")
 public class JpaAddressBookRepository implements AddressBookRepository {
 
+    private final SimpleJpaRepository<AddressBook, CustomerId> jpaRepository;
+
+    public JpaAddressBookRepository(EntityManager entityManager) {
+        this.jpaRepository = new SimpleJpaRepository<>(AddressBook.class, entityManager);
+    }
+
     @Override
     public void add(AddressBook addressBook) {
-
+        jpaRepository.save(addressBook);
     }
 
     @Override
